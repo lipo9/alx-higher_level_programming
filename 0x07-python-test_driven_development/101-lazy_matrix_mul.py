9cc9487fc40fa96a549d8806afafe209ef3956d2
Abderrahim Libourki <abdolibourki@gmail.com>
2023-01-09 22:47:44 +0100

    (Complete) 0x0A. Python - Inheritance

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -21,4 +21,3 @@ def lazy_matrix_mul(m_a, m_b):
     """
 
     return (np.matmul(m_a, m_b))
-
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Definition of the class here
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    pass
